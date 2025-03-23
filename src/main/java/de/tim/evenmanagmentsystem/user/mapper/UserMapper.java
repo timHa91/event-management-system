27f9f0e0f785368564502d0f7ced4a4496339269
timHa91 <tim.andre.hartmann@gmail.com>
2025-03-23 12:48:46 +0100

    Added Mapstruct and jwt dependencies and UserMapper

---
+++ src/main/java/de/tim/evenmanagmentsystem/user/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package de.tim.evenmanagmentsystem.user.mapper;
+
+import de.tim.evenmanagmentsystem.user.dto.UserDTO;
+import de.tim.evenmanagmentsystem.user.dto.UserRegistrationDTO;
+import de.tim.evenmanagmentsystem.user.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    UserDTO toDTO(User user);
+
+    @Mapping(target = "password", ignore = true)
+    User toEntity(UserRegistrationDTO dto);
+}
