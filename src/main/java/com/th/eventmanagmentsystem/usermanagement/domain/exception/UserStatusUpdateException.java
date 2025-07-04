8b13d7900b3f3d42f08be235431b266071e2c452
timHa91 <tim.andre.hartmann@gmail.com>
2025-07-04 20:35:36 +0200

    feature: UserStatusUpdateException erstellt

---
+++ src/main/java/com/th/eventmanagmentsystem/usermanagement/domain/exception/UserStatusUpdateException.java
@@ -0,0 +1,9 @@
+package com.th.eventmanagmentsystem.usermanagement.domain.exception;
+
+public class UserStatusUpdateException extends RuntimeException{
+
+    public UserStatusUpdateException(String msg) {
+        super(msg);
+    }
+
+}
