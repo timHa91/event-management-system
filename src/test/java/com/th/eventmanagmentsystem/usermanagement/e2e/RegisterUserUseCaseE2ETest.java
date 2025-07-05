152b40a2b26c9df2597fae278ff8c675cd369062
timHa91 <tim.andre.hartmann@gmail.com>
2025-07-05 14:34:26 +0200

    fix: Refactoring für Testing

--- src/test/java/com/th/eventmanagmentsystem/usermanagement/e2e/RegisterUserUseCaseE2ETest.java
+++ src/test/java/com/th/eventmanagmentsystem/usermanagement/e2e/RegisterUserUseCaseE2ETest.java
@@ -1,4 +1,22 @@
 package com.th.eventmanagmentsystem.usermanagement.e2e;
 
+import com.th.eventmanagmentsystem.usermanagement.domain.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Transactional
 public class RegisterUserUseCaseE2ETest {
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Autowired
+    private UserRepository userRepository;
+
+
 }
--- target/generated-sources/annotations/com/th/eventmanagmentsystem/usermanagement/application/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/com/th/eventmanagmentsystem/usermanagement/application/mapper/UserMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-07-05T12:44:45+0200",
+    date = "2025-07-05T13:50:54+0200",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.5 (Amazon.com Inc.)"
 )
 @Component
